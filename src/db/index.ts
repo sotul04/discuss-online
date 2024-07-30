b3d29d7ed0809921def4cd82139a49b14425bf89
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-07-31 05:15:27 +0700

    fix: fix duplicate db instances

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient();
+const prismaClientSingleton = () => {
+    return new PrismaClient()
+}
+
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+const db = prisma;
 
 export default db;
