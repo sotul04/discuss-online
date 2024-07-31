de463f177c977a7d296c4db59c47062ac9f8b025
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-07-31 20:10:55 +0700

    fix: fix search page

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -15,7 +15,7 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
         redirect('/');
     }
 
-    return <div>
+    return <div className="space-y-3 container h-screen">
         <h3 className="text-xl pl-2 my-8">Search: <code className="font-bold">{term}</code></h3>
         <PostList fetchData={() => fetchPostsBySearchTerm(term)} />
     </div>
